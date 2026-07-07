7b1a4caee2076f35b0668021fa49de7252488acd
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-07-07 19:24:23 -0300

    feat: add header component and custom breakpoint for larger screensizes

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="mb-8 text-center ">
+      <h1 className="text-2xl sm:text-3xl lgscreen:text-4xl font-bold text-white tracking-tight">
+        Gerenciador de Tarefas
+      </h1>
+    </header>
+  )
+}
+
+export default Header
